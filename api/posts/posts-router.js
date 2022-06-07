8743baef2c3f06ca345be7ba942cace24611950c
Albert Szabo <albertszabo@Alberts-MacBook-Pro.local>
2022-06-07 13:02:06 -0700

    Added GET endpoint to retrieve all posts

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -6,6 +6,16 @@ const Posts = require('./posts-model');
 
 const router = express.Router();
 
-
+router.get('/', (request, response) => {
+    Posts.find()
+        .then(posts => {
+            response.json(posts);
+        })
+        .catch(error => {
+            response.status(500).json({
+                message: 'The posts information could not be retrieved'
+            });
+        });
+});
 
 module.exports = router;
